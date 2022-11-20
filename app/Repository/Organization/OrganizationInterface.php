a281b05a78d4a1698678b16f2e72c80b99f19e23
Dip Ghosh <dip@storat.com>
2022-11-20 23:01:28 +0600

    seeding done for organization

---
+++ app/Repository/Organization/OrganizationInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repository\Organization;
+
+interface OrganizationInterface
+{
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\Backend\Auth\LoginController;
 use App\Http\Controllers\Backend\Job\JobController;
-use App\Http\Controllers\Backend\Job\JobTypeController;
+use App\Http\Controllers\OrganizationController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -10,7 +10,7 @@ Route::post('dashboard', [LoginController::class, 'login'])->name('login');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-    Route::resource('job-types', JobTypeController::class);
+    Route::resource('organizations', OrganizationController::class);
     Route::resource('jobs', JobController::class);
     Route::get('applicant-details/{id}', [JobController::class, 'getApplicantDetails'])->name('applicant-details');
 });
