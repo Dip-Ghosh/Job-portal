e034a0d4ca11cc6b40c5463fb04ce0b3c70fc21e
vagrant <vagrant@homestead>
2022-11-22 19:08:44 +0000

    location seeder added

---
+++ database/seeders/LocationSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Location;
+use Illuminate\Database\Seeder;
+
+class LocationSeeder extends Seeder
+{
+
+    public function run()
+    {
+        $locations = [
+            "Dhaka", "Faridpur", "Gazipur", "Gopalganj", "Jamalpur", "Kishoreganj", "Madaripur", "Manikganj", "Munshiganj",
+            "Mymensingh", "Narayanganj", "Narsingdi", "Netrokona", "Rajbari", "Shariatpur", "Sherpur", "Bogura", "Joypurhat",
+            "Naogaon", "Natore", "Nawabganj", "Pabna", "Rajshahi", "Sirajgonj", "Dinajpur", "Gaibandha", "Kurigram",
+            "Lalmonirhat", "Nilphamari", "Panchagarh", "Rangpur", "Thakurgaon", "Barguna", "Barishal", "Bhola",
+            "Jhalokati", "Patuakhali", "Pirojpur", "Bandarban", "Brahmanbaria", "Chandpur", "Chattogram", "Lakshmipur",
+            "Cumilla", "Cox's Bazar", "Feni", "Khagrachari", "Noakhali", "Rangamati", "Habiganj", "Maulvibazar", "Sunamganj",
+            "Sylhet", "Bagerhat", "Chuadanga", "Jashore", "Jhenaidah", "Khulna", "Kushtia", "Magura", "Meherpur", "Narail", "Satkhira",
+        ];
+
+        foreach ($locations as $location) {
+            Location::create(['industry_type' => $location]);
+            $this->command->info($location . " has created successfully");
+        }
+    }
+}
