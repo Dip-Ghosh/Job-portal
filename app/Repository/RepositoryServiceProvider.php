56f502551baacad9615fe650b58279b3a3759ca7
vagrant <vagrant@homestead>
2022-11-23 18:39:44 +0000

    model scope added for job title

--- app/Repository/RepositoryServiceProvider.php
+++ app/Repository/RepositoryServiceProvider.php
@@ -7,11 +7,7 @@ use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
+
     public function register()
     {
         $this->app->bind('App\Repository\Base\WriteAbleInterface', BaseRepository::class);
@@ -20,6 +16,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind('App\Repository\Organization\OrganizationInterface', 'App\Repository\Organization\OrganizationRepository');
         $this->app->bind('App\Repository\Industry\IndustryInterface', 'App\Repository\Industry\IndustryRepository');
         $this->app->bind('App\Repository\Location\LocationInterface', 'App\Repository\Location\LocationRepository');
+        $this->app->bind('App\Repository\JobTitle\JobTitleInterface', 'App\Repository\JobTitle\JobTitleRepository');
 
         $this->app->bind('App\Repository\Job\JobInterface', 'App\Repository\Job\JobRepository');
         $this->app->bind('App\Repository\Api\LoginRegistrationInterface', 'App\Repository\Api\LoginRegistrationRepository');
@@ -27,11 +24,6 @@ class RepositoryServiceProvider extends ServiceProvider
 
     }
 
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
     public function boot()
     {
         //
