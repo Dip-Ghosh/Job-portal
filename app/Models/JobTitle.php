d7a187b671797f9b05796a681267763abd4dcf65
vagrant <vagrant@homestead>
2022-11-23 18:39:44 +0000

    model scope added for job title

--- app/Models/JobTitle.php
+++ app/Models/JobTitle.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class JobTitle extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'status'];
+
+    public function scopeActive($query)
+    {
+        return $query->where('status', 1);
+    }
+
+    public function scopeOrders($query)
+    {
+        return $query->orderBy('id', 'desc');
+    }
 }
