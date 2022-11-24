56b3b109b53ec3526e2677b9a022df56e87ea72b
Dip Ghosh <dip@storat.com>
2022-11-24 23:24:44 +0600

    migration created

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'organizations_id', 'industries_id', 'address', 'mobile', 'email', 'business_info', 'logo',
+        'web_url', 'status',
+    ];
+
+    public function scopeActive($query)
+    {
+        return $query->where('status', 1);
+    }
+
+    public function scopeOrders($query)
+    {
+        return $query->orderBy('id', 'desc');
+    }
+
+    
 }
