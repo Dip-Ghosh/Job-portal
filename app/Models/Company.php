c2092c4a9106232892b512bababe73817c700643
Dip Ghosh <dip@storat.com>
2022-11-27 20:47:59 +0600

    one to one relation define in company and organization and industyr

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Company extends Model
 {
@@ -24,5 +25,15 @@ class Company extends Model
         return $query->orderBy('id', 'desc');
     }
 
-    
+    public function organization(): belongsTo
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+    public function industry(): belongsTo
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+
 }
