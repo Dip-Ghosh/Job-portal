0cfc3bd639dee2628585adc637a8c3ff47927516
Dip Ghosh <dip@storat.com>
2022-11-28 12:03:44 +0600

    database js remove

--- app/Repository/Backend/OrganizationRepository.php
+++ app/Repository/Backend/OrganizationRepository.php
@@ -8,7 +8,7 @@ use App\Repository\Base\BaseRepository;
 class OrganizationRepository extends BaseRepository implements OrganizationInterface
 {
     protected $organization;
-
+    
     public function __construct(Organization $organization)
     {
         parent::__construct($organization);
--- resources/views/backend/job-title/list.blade.php
+++ resources/views/backend/job-title/list.blade.php
@@ -56,5 +56,4 @@
             </div>
 
         </div>
-    @include('.js.datatable')
 @endsection
