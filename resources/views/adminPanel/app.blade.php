e35ea0a62b0713367c4da7bcc64d73e23bf5bb58
Dip Ghosh <dip@storat.com>
2022-11-28 16:01:27 +0600

    cdn remove

--- resources/views/adminPanel/app.blade.php
+++ resources/views/adminPanel/app.blade.php
@@ -23,8 +23,6 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 
-    <!-- select2 -->
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
 </head>
 
 <body id="page-top">
@@ -113,8 +111,7 @@
 
 <!-- Page level custom scripts -->
 <script src="{{asset('vendor/js/demo/datatables-demo.js')}}"></script>
-<!-- select2 script -->
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
