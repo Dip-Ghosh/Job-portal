575c9ebd6009df86b163950cd5e170756511d47d
Dip Ghosh <dip@storat.com>
2022-11-30 00:16:49 +0600

    split the provider

--- app/Repository/RepositoryServiceProvider.php
+++ app/Repository/RepositoryServiceProvider.php
@@ -13,15 +13,23 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind('App\Repository\Base\WriteAbleInterface', BaseRepository::class);
         $this->app->bind('App\Repository\Base\ReadAbleInterface', BaseRepository::class);
 
-        $this->app->bind('App\Repository\Backend\OrganizationInterface', 'App\Repository\Backend\OrganizationRepository');
-        $this->app->bind('App\Repository\Backend\IndustryInterface', 'App\Repository\Backend\IndustryRepository');
-        $this->app->bind('App\Repository\Backend\LocationInterface', 'App\Repository\Backend\LocationRepository');
-        $this->app->bind('App\Repository\Backend\JobTitleInterface', 'App\Repository\Backend\JobTitleRepository');
-        $this->app->bind('App\Repository\Backend\CompanyInterface', 'App\Repository\Backend\CompanyRepository');
+        $this->app->bind('App\Repository\Backend\OrganizationInterface',
+            'App\Repository\Backend\OrganizationRepository');
+        $this->app->bind('App\Repository\Backend\IndustryInterface',
+            'App\Repository\Backend\IndustryRepository');
+        $this->app->bind('App\Repository\Backend\LocationInterface',
+            'App\Repository\Backend\LocationRepository');
+        $this->app->bind('App\Repository\Backend\JobTitleInterface',
+            'App\Repository\Backend\JobTitleRepository');
+        $this->app->bind('App\Repository\Backend\CompanyInterface',
+            'App\Repository\Backend\CompanyRepository');
 
-        $this->app->bind('App\Repository\Job\JobInterface', 'App\Repository\Job\JobRepository');
-        $this->app->bind('App\Repository\Api\LoginRegistrationInterface', 'App\Repository\Api\LoginRegistrationRepository');
-        $this->app->bind('App\Repository\JobApplication\JobApplicationInterface', 'App\Repository\JobApplication\JobApplicationRepository');
+        $this->app->bind('App\Repository\Job\JobInterface',
+            'App\Repository\Job\JobRepository');
+        $this->app->bind('App\Repository\Api\LoginRegistrationInterface',
+            'App\Repository\Api\LoginRegistrationRepository');
+        $this->app->bind('App\Repository\JobApplication\JobApplicationInterface',
+            'App\Repository\JobApplication\JobApplicationRepository');
 
     }
 
